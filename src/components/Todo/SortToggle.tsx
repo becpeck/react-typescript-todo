d2902efc888bb3d60ab5e2cb3b686cab315298e9
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-10-21 12:47:53 -0700

    sort toggle styled

--- src/components/Todo/SortToggle.tsx
+++ src/components/Todo/SortToggle.tsx
@@ -6,17 +6,21 @@ export default function SortToggle(props: SortToggleProps) {
   const { sortOn, toggleSort } = props;
 
   return (
-    <div>
-      <input 
-        type='checkbox' 
-        name='sort-toggle' 
-        id='sort-toggle' 
-        checked={sortOn} 
-        onChange={toggleSort}
-      />
-      <label htmlFor='sort-toggle'>
-        Sort
-      </label>
+    <div className='toggle'>
+      <label htmlFor='sort-toggle' className='toggle-label'>Sort</label>
+      <div>
+        <input 
+          className='toggle-input'
+          type='checkbox' 
+          name='sort-toggle' 
+          id='sort-toggle' 
+          checked={sortOn} 
+          onChange={toggleSort}
+        />
+        <label htmlFor='sort-toggle' className='toggle-track'>
+          <span className='toggle-slider'/>
+        </label>
+      </div>
     </div>
   );
 }
