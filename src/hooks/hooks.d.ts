48025ba0392aa9983d92e3c48bf114a6b7197564
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-11-18 16:03:06 -0800

    useTaskState and useSortState functions added, implement useLocalStorage function with specific types, return types for both added

--- src/hooks/hooks.d.ts
+++ src/hooks/hooks.d.ts
@@ -1,1 +1,5 @@
+import { Task } from '../components/Todo/TodoList.interface';
+
 export type UseLocalStorage<T> = [T, (value: T) => void];
+export type UseTaskState = UseLocalStorage<Task[]>;
+export type UseSortState = UseLocalStorage<boolean>;
--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 
-import { UseLocalStorage } from './hooks';
+import { Task } from '../components/Todo/TodoList.interface';
+import { UseLocalStorage, UseTaskState, UseSortState } from './hooks';
 
 function useLocalStorage<T>(key: string, initialValue: T): UseLocalStorage<T> {
     const [storedValue, setStoredValue] = useState<T>(() => {
@@ -14,4 +15,12 @@ function useLocalStorage<T>(key: string, initialValue: T): UseLocalStorage<T> {
     }
 
     return [storedValue, setValue];
+}
+
+export function useTaskState(initialTasks: Task[]): UseTaskState {
+    return useLocalStorage<Task[]>('tasks', initialTasks)
+}
+
+export function useSortState(initialSortOn: boolean): UseSortState {
+    return useLocalStorage<boolean>('sortOn', initialSortOn)
 }
