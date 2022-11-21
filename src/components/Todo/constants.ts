d5038c39b49ec12d411cdafa1f51d44710705281
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-11-21 14:24:39 -0800

    constants.ts cleanup: removed extraneous `as const`, object formatting

--- src/components/Todo/constants.ts
+++ src/components/Todo/constants.ts
@@ -1,20 +1,58 @@
 import { v4 as uuid } from 'uuid';
 import { Task, NewTask } from './TodoList.interface';
 
-export const ICONS = {
-    EMPTY_BOX: { ICON: '\u2610', CLASS_NAME: 'checkbox' } as const,
-    CHECKED_BOX: { ICON: '\u2612', CLASS_NAME: 'checkbox' } as const,
-    PENCIL: { ICON: '\u270F', CLASS_NAME: 'pencil' } as const,
-    X_REMOVE: { ICON: '\u2715', CLASS_NAME: 'x-remove' } as const,
-    PLUS: { ICON: '\uFF0B', CLASS_NAME: 'plus' } as const,
-    NO_ICON: { ICON: '', CLASS_NAME: 'no-icon' } as const
-} as const;
-
 export const initialNewTaskInput: NewTask = { text: '', editOn: false };
 
 export const sampleTasks: Task [] = [
-    { id: uuid(), text: 'TS conversion', completed: true, editOn: false },
-    { id: uuid(), text: 'Make lunch', completed: false, editOn: false },
-    { id: uuid(), text: 'Clean kitchen', completed: true, editOn: false },
-    { id: uuid(), text: 'Finish laundry', completed: false, editOn: false },
+    {
+        id: uuid(),
+        text: 'TS conversion',
+        completed: true,
+        editOn: false
+    },
+    {
+        id: uuid(),
+        text: 'Make lunch',
+        completed: false,
+        editOn: false
+    },
+    {
+        id: uuid(),
+        text: 'Clean kitchen',
+        completed: true,
+        editOn: false
+    },
+    {
+        id: uuid(),
+        text: 'Finish laundry',
+        completed: false,
+        editOn: false
+    }
 ];
+
+export const ICONS = {
+    EMPTY_BOX: {
+        ICON: '\u2610',
+        CLASS_NAME: 'checkbox'
+    },
+    CHECKED_BOX: {
+        ICON: '\u2612',
+        CLASS_NAME: 'checkbox'
+    },
+    PENCIL: {
+        ICON: '\u270F',
+        CLASS_NAME: 'pencil'
+    },
+    X_REMOVE: {
+        ICON: '\u2715',
+        CLASS_NAME: 'x-remove'
+    },
+    PLUS: {
+        ICON: '\uFF0B',
+        CLASS_NAME: 'plus'
+    },
+    NO_ICON: {
+        ICON: '',
+        CLASS_NAME: 'no-icon'
+    }
+} as const;
