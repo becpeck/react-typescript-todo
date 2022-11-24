d3f41ce0a0d728968d4aebb88be0cce06446c087
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-11-23 19:06:06 -0800

    nested icon scss

--- src/components/Todo/Icon.tsx
+++ src/components/Todo/Icon.tsx
@@ -6,6 +6,6 @@ export default function Icon(props: IconProps) {
     const { variant, handleClick } = props;
 
     return (
-        <span className={variant.CLASS_NAME} onClick={handleClick}>{variant.ICON}</span>
+        <span className={`icon ${variant.CLASS_NAME}`} onClick={handleClick}>{variant.ICON}</span>
     );
 }
