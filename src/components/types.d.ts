270a4f99bccddfb8db22ca4415d84ecc9ddaa1b0
Rebecca Peckman <rebeccapeckman@gmail.com>
2022-12-08 21:22:25 -0800

    converted interfaces in types.d.ts to types

--- src/components/types.d.ts
+++ src/components/types.d.ts
@@ -1,18 +1,18 @@
 import { THEME_MODES, THEME_COLORS } from "./constants";
 
-export interface ThemeColor {
+export type ThemeColor = {
     color: typeof THEME_COLORS[keyof typeof THEME_COLORS];
     active: boolean
 }
 
 export type ThemeMode = typeof THEME_MODES[keyof typeof THEME_MODES];
 
-export interface NewTask {
+export type NewTask = {
     text: string;
     editOn: boolean;
 }
 
-export interface Task extends NewTask {
+export type Task = NewTask & {
     id: string;
     completed: boolean;
 }
